977c84c79cfc4d35a8c386f393a203f57e444188
Vitaliy-Maksymchuk <130071443+Vitaliy-Maksymchuk@users.noreply.github.com>
2024-11-18 21:36:03 +0000

    Deploying to gh-pages from @ Vitaliy-Maksymchuk/goit-js-hw-09@ae81dcca4c11a99bbc252539622939891345d9a1 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B4s-4mBC.js";console.log("Form");
+import"./assets/styles-BitBOp5E.js";const e={email:"",message:""},a=document.querySelector(".feedback-form"),s="feedback-form-state",m=a.elements.email,o=a.elements.message;a.addEventListener("input",i);a.addEventListener("submit",r);function n(){const t=localStorage.getItem(s);if(t){const l=JSON.parse(t);e.email=l.email||"",e.message=l.message||"",m.value=e.email,o.value=e.message}}function i(t){e[t.target.name]=t.target.value.trim(),localStorage.setItem(s,JSON.stringify(e))}function r(t){if(t.preventDefault(),!e.email||!e.message)return alert("Fill please all fields");console.log(e),localStorage.removeItem(s),e.email="",e.message="",a.reset()}n();
 //# sourceMappingURL=2-form.js.map
